a5c957ac54000e8a9b28814c2530eca4b678f8b6
Rajesh Singh Bohra <siinghraajesh@gmail.com>
2024-12-31 22:22:17 +0530

    Triangle pattern coded

--- Patterns/patterns.cpp
+++ Patterns/patterns.cpp
@@ -110,4 +110,30 @@ cout << "----------------" << endl;
         space--;
         cout << endl;
     }
+
+    cout << "----------------" << endl;
+
+     /*
+              1
+            1 2 1
+          1 2 3 2 1
+        1 2 3 4 3 2 1  
+    */
+
+   for(char i=1; i<=len; i++) {
+        int space = len-i;
+        for(int j=1; j<=space; j++) {
+            cout << " " << " ";
+        }
+        for(int j=1; j<=i; j++) {
+            cout << j << " ";
+        }
+        if (i>1) {
+            for(int j=i-1; j>0; j--) {
+                cout << j << " ";
+            }
+        }
+        space--;
+        cout << endl;
+    }
 }
